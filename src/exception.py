2c833d3aa269607a606f052fd686050051b1c140
Soumojit <gsshacker608@gmail.com>
2026-03-02 15:38:15 +0530

    updated exception.py

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 def error_message_details(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -21,4 +22,5 @@ class CustomException(Exception):
 #     try:
 #         a = 1 / 0
 #     except Exception as e:
-#         logging.error(CustomException(e, sys))
+#         logging.info("divide by 0")
+#         raise CustomException(e,sys)
